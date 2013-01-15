54174dd37d18e2423ed093c4e3b56ca29e44825c
huydang1920@gmail.com <huydang1920@gmail.com@d468154a-861e-cf32-b975-07f2667dc1a1>
2013-01-15 10:14:47 +0000

    Cập nhật AuthorCitation.

--- lucene/src/indexer/AuthorCitationIndexer.java
+++ lucene/src/indexer/AuthorCitationIndexer.java
@@ -17,16 +17,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.NumericField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -53,43 +55,35 @@ public class AuthorCitationIndexer {
         return out;
     }
 
+    /**
+     * Truy vấn các thông tin authorCitation từ csdl thực hiện index
+     *
+     * @param connectionPool: kết nối csdl
+     * @param indexDir: thư mục lưu trữ file index
+     * @return số doc thực hiện
+     */
     private int _index(ConnectionPool connectionPool) throws IOException, SQLException {
         int count = 0;
         StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_36);
         IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_36, analyzer);
         Directory directory = FSDirectory.open(new File(path + IndexConst.AUTHOR_CITATION_DIRECTORY_PATH));
         IndexWriter writer = new IndexWriter(directory, config);
-        // Connection to DB
-        Connection connection = connectionPool.getConnection();
         try {
-            String query = "SELECT " + AuthorTB.COLUMN_AUTHORID + " FROM " + AuthorTB.TABLE_NAME + " a";
-            PreparedStatement stmt = connection.prepareStatement(query, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setFetchSize(Integer.MIN_VALUE);
-            ResultSet rs = stmt.executeQuery();
-            // Index data from query
-            while ((rs != null) && (rs.next())) {
-                ArrayList<Object> object = this.getObjects(connectionPool, rs.getInt(AuthorTB.COLUMN_AUTHORID));
-                if (object == null) {
+            for (int i = 1; i <= this.getMaxIdAuthor(connectionPool); i++) {
+                String cAuthors = this.cAuthors(connectionPool, i);
+                if (cAuthors == null) {
                     continue;
                 }
-                //idAuthor - cidAuthor - citationCount
-                for (int i = 0; i < object.size(); i++) {
-                    LinkedHashMap<String, Object> item = (LinkedHashMap<String, Object>) object.get(i);
-                    Document d = new Document();
-                    d.add(new Field("idAuthor", rs.getString(AuthorTB.COLUMN_AUTHORID), Field.Store.YES, Field.Index.ANALYZED));
-                    d.add(new Field("cidAuthor", item.get("idAuthor").toString(), Field.Store.YES, Field.Index.NO));
-                    d.add(new NumericField("citationCount", Field.Store.YES, true).setIntValue(Integer.parseInt(item.get("citationCount").toString())));
-                    writer.addDocument(d);
-                    d = null;
-                    System.out.println("Indexing: " + count++ + "\t" + " idAuthor: " + rs.getString(AuthorTB.COLUMN_AUTHORID) + "\t" + " cidAuthor: " + item.get("idAuthor").toString() + "\t" + " citationCount: " + Integer.parseInt(item.get("citationCount").toString()));
-                }
+                Document d = new Document();
+                d.add(new Field("idAuthor", Integer.toString(i), Field.Store.YES, Field.Index.ANALYZED));
+                d.add(new Field("cAuthors", cAuthors, Field.Store.YES, Field.Index.NO));
+                writer.addDocument(d);
+                d = null;
+                System.out.println("Indexing: " + count++ + "\t" + " idAuthor: " + Integer.toString(i));
             }
-            rs.close();
-            stmt.close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         } finally {
-            connection.close();
             writer.optimize();
             writer.close();
             directory.close();
@@ -97,24 +91,63 @@ public class AuthorCitationIndexer {
         return count;
     }
 
-    private ArrayList<Object> getObjects(ConnectionPool connectionPool, int idAuthor) throws SQLException {
-        ArrayList<Object> out = new ArrayList<Object>();
+    /**
+     * Lấy idAuthor lớn nhất trong database
+     *
+     * @author HuyDang
+     * @param connectionPool
+     * @return
+     * @throws SQLException
+     */
+    private int getMaxIdAuthor(ConnectionPool connectionPool) throws SQLException {
+        int id = 0;
+        // Connection to DB
+        Connection connection = connectionPool.getConnection();
+        try {
+            String sql = "SELECT " + AuthorTB.COLUMN_AUTHORID + " FROM " + AuthorTB.TABLE_NAME + " a ORDER BY " + AuthorTB.COLUMN_AUTHORID + " DESC LIMIT 1";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            if ((rs != null) && (rs.next())) {
+                id = rs.getInt(AuthorTB.COLUMN_AUTHORID);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            connection.close();
+        }
+        return id;
+    }
+
+    /**
+     * Lấy danh sách các tác giả mà idAuthor tham khảo
+     *
+     * @author HuyDang
+     * @param connectionPool
+     * @param idAuthor
+     * @return
+     * @throws SQLException
+     */
+    private String cAuthors(ConnectionPool connectionPool, int idAuthor) throws SQLException {
+        ArrayList<Object> cAuthors = new ArrayList<Object>();
+        Map json = new HashMap();
         Connection connection = connectionPool.getConnection();
         try {
             String list = this.getListIdPaperFromAuthor(connectionPool, idAuthor);
-            if (!"".equals(list)) {
+            if (!"".equals(list.trim())) {
                 String sql = "SELECT ap." + AuthorPaperTB.COLUMN_AUTHORID + ", COUNT(ap." + AuthorPaperTB.COLUMN_AUTHORID + ") AS citationCount "
                         + "FROM " + PaperPaperTB.TABLE_NAME + " pp JOIN " + AuthorPaperTB.TABLE_NAME + " ap ON (ap." + AuthorPaperTB.COLUMN_PAPERID + " = pp." + PaperPaperTB.COLUMN_PAPERID + ") "
                         + "WHERE pp." + PaperPaperTB.COLUMN_PAPERREFID + " IN (" + list + ") AND ap." + AuthorPaperTB.COLUMN_AUTHORID + " <> ? "
-                        + "GROUP BY ap." + AuthorPaperTB.COLUMN_AUTHORID + " ORDER BY citationCount DESC LIMIT 20";
+                        + "GROUP BY ap." + AuthorPaperTB.COLUMN_AUTHORID + " ORDER BY citationCount DESC LIMIT 30";
                 PreparedStatement stmt = connection.prepareStatement(sql);
                 stmt.setInt(1, idAuthor);
                 ResultSet rs = stmt.executeQuery();
                 while ((rs != null) && (rs.next())) {
-                    LinkedHashMap<String, Object> item = new LinkedHashMap<String, Object>();
+                    LinkedHashMap<String, Integer> item = new LinkedHashMap<String, Integer>();
                     item.put("idAuthor", rs.getInt(AuthorPaperTB.COLUMN_AUTHORID));
                     item.put("citationCount", rs.getInt("citationCount"));
-                    out.add(item);
+                    cAuthors.add(item);
                 }
                 rs.close();
                 stmt.close();
@@ -124,9 +157,23 @@ public class AuthorCitationIndexer {
         } finally {
             connection.close();
         }
-        return out;
+        if (cAuthors.size() > 0) {
+            json.put("cAuthors", cAuthors);
+            JSONObject outJSON = new JSONObject(json);
+            return outJSON.toJSONString();
+        }
+        return null;
     }
 
+    /**
+     * Lấy danh sách idPaper của 1 tác giả
+     *
+     * @author HuyDang
+     * @param connectionPool
+     * @param idAuthor
+     * @return
+     * @throws SQLException
+     */
     private String getListIdPaperFromAuthor(ConnectionPool connectionPool, int idAuthor) throws SQLException {
         String list = "";
         Connection connection = connectionPool.getConnection();
